c3e642b642f2e53bfa62e275c4b17d712f906edf
Zynh0722 <Zynh0722@gmail.com>
2023-08-20 22:21:55 -0700

    players?

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,28 @@
 #[derive(Debug)]
 pub struct Board {
     pub nodes: [[Option<WallDirection>; 8]; 8],
+    pub players: Vec<Player>,
+}
+
+#[derive(Debug)]
+pub struct Player {
+    pub pos: PlayerPosition,
+    pub walls_remaining: u8,
+}
+
+impl Player {
+    pub fn new(pos: PlayerPosition, total_players: u8) -> Self {
+        Self {
+            pos,
+            walls_remaining: 20 / total_players,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct PlayerPosition {
+    pub x: usize,
+    pub y: usize,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -8,6 +30,13 @@ pub enum WallDirection {
     Vertical,
     Horizontal,
 }
+
+static DEFAULT_POSITIONS: [PlayerPosition; 4] = [
+    PlayerPosition { x: 4, y: 0 },
+    PlayerPosition { x: 4, y: 8 },
+    PlayerPosition { x: 0, y: 4 },
+    PlayerPosition { x: 8, y: 4 },
+];
 
 impl WallDirection {
     #[inline]
@@ -25,9 +54,17 @@ impl WallDirection {
 }
 
 impl Board {
-    pub fn new() -> Self {
+    pub fn new(num_players: u8) -> Self {
+        assert!(num_players <= 4);
+        assert!(num_players >= 2);
+
+        let mut players = Vec::new();
+        for i in 0..num_players {
+            players.push(Player::new(DEFAULT_POSITIONS[i as usize], num_players))
+        }
         Self {
             nodes: [[None; 8]; 8],
+            players,
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,6 @@ impl Printable for quoridor::Board {
                     use quoridor::WallDirection::{Horizontal, Vertical};
                     let x = 4 * x + 4;
                     let y = 2 * y + 2;
-                    println!("{}, {}, {:?}", x, y, direction);
                     match direction {
                         Vertical => {
                             board_strings[y + 1].replace_range(x..x + 1, "|");
@@ -39,12 +38,19 @@ impl Printable for quoridor::Board {
             }
         }
 
+        for (id, player) in self.players.iter().enumerate() {
+            let x = 4 * player.pos.x + 2;
+            let y = 2 * player.pos.y + 1;
+
+            board_strings[y].replace_range(x..x + 1, &id.to_string())
+        }
+
         board_strings.iter().for_each(|s| println!("{}", s));
     }
 }
 
 fn main() {
-    let mut board = quoridor::Board::new();
+    let mut board = quoridor::Board::new(2);
     board.place_wall(0, 0, quoridor::WallDirection::Vertical);
     board.place_wall(2, 3, quoridor::WallDirection::Horizontal);
     board.print()
